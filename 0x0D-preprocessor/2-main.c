b0df3c3fb151531c6441473fd0c8aa1d664aecf9
12344567891011 <asmae.tahtouh@usmba.ac.ma>
2023-06-21 05:13:06 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  * main - current file
+ *   Description: here we go
+ *   Return: name of current file
+ */
+int main(void)
+{
+printf("%s\n",__FILE__);
+return (0);
+}
