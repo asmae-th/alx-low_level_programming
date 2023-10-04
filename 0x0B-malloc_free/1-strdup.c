a5c06f3bb5a28f3a68589d4df11eb10580e01d2c
asma <117948441+12344567891011@users.noreply.github.com>
2023-10-04 12:59:13 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,28 +9,25 @@
  *      */
 char *_strdup(char *str)
 {
-		char *s;
-			unsigned int i;
-				unsigned int len = 0;
+	char *s;
+	unsigned int i;
+	unsigned int len = 0;
 
-					if (str == NULL)
-							{
-										return (NULL);
-											}
-						while (str[len] != '\0')
-								{
-											len++;
-												}
-							s = malloc(sizeof(char) * (len + 1));
-
-								if (s == NULL)
-										{
-													return (NULL);
-														}
-									for (i = 0; i < len; i++)
-											{
-														s[i] = str[i];
-															}
-										s[i] = '\0';
-											return (s);
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	s = malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = '\0';
+	return (s);
 }
