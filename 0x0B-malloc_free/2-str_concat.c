f89769448938a8ebb725aacd170fb5bdce1ac5d6
asma <117948441+12344567891011@users.noreply.github.com>
2024-02-22 21:27:12 +0000

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,23 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+ /**
+ *  * str_concat - str function
+ *   * @str: str
+ *    *
+ *     * Return: NULL if str is NULL or insufficient memory. Otherwise pointer
+ *
+ */
 char *str_concat(char *s1, char *s2)
 {
     int i = 0;
     int j = 0;
-    char *ptr =(char *)malloc((strlen(s1) + strlen(s2)) * sizeof(char));
+    char *str =(char *)malloc((strlen(s1) + strlen(s2)) * sizeof(char));
+    if(!str)
+        return NULL;
     while(s1[i] != '\0')
     {
-        ptr[i] = s1[i];
+        str[i] = s1[i];
         i++;
     }
     while(s2[j] != '\0')
     {
-        ptr[i] = s2[j];
+        str[i] = s2[j];
         i++;
         j++;
     }
-    ptr[i] = '\0';
-    return ptr;
+    str[i] = '\0';
+    return str;
 }
