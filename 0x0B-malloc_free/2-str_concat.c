a8ce5910e27d3c9bd97bb1a692ed47bd7b68a512
asma <117948441+12344567891011@users.noreply.github.com>
2024-02-22 22:09:12 +0000

    change

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,33 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- /**
+/**
  *  * str_concat - str function
  *   * @str: str
  *    *
  *     * Return: NULL if str is NULL or insufficient memory. Otherwise pointer
  *
  */
-char *str_concat(char *s1, char *s2)
+char	*str_concat(char *s1, char *s2)
 {
-    int i = 0;
-    int j = 0;
-    char *str =(char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
-    if(!str)
-        return NULL;
-    if(!s1 && !s2)
-        return "";
-    while(s1[i] != '\0')
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while(s2[j] != '\0')
-    {
-        str[i] = s2[j];
-        i++;
-        j++;
-    }
-    str[i] = '\0';
-    return str;
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *) malloc ((strlen (s1) + strlen (s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	if (!s1 && !s2)
+		return ("");
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
 }
