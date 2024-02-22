ff2674a2599b84555dadf5c422a58a323a5fbfb4
asma <117948441+12344567891011@users.noreply.github.com>
2024-02-22 21:24:23 +0000

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,8 +8,6 @@ char *str_concat(char *s1, char *s2)
     int i = 0;
     int j = 0;
     char *ptr =(char *)malloc((strlen(s1) + strlen(s2)) * sizeof(char));
-    // if(!s1 && !s2)
-    //     return NULL;
     while(s1[i] != '\0')
     {
         ptr[i] = s1[i];
