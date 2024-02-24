4824906bad66ac007532966983cae16db4ee4fe1
asma <117948441+12344567891011@users.noreply.github.com>
2024-02-24 13:04:26 +0000

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - print the code
+ *
+ * Return:  the number of nodes.
+ */
+
+size_t	print_list(const list_t *h)
+{
+	int	i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		printf ("[%d]   %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
